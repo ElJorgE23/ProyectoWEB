0713b931b8fa4843e5a7a22524265ed3024dab3d
ElJorgE23 <s21120186@alumnos.itsur.edu.mx>
2025-03-15 22:15:22 -0600

    Primera parte del proyecto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Componente from './Componente';
+import './Componente.css';
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Componente></Componente>
       </header>
     </div>
   );
